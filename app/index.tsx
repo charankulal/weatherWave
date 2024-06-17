d9640752b9ec6a8d289c1cb7037c00564997c71a
Charan <charankulal0241@gmail.com>
2024-06-17 17:15:43 +0530

    Designed the basic layout which includes current weather details and forecasting details for next 7 days.

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
-import { View, Image, TextInput, TouchableOpacity, Text } from "react-native";
+import { View, Image, TextInput, TouchableOpacity, Text, ScrollView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { MagnifyingGlassCircleIcon } from "react-native-heroicons/outline";
-import { MapPinIcon } from "react-native-heroicons/solid";
+import { CalendarDaysIcon, MapPinIcon } from "react-native-heroicons/solid";
 
 const Home = () => {
   const [showSearch, toggleSearch] = useState(false);
-  const [locations, setLocations] = useState([1, 2, 3])
+  const [locations, setLocations] = useState([1, 2, 3]);
+  const handleLocation = (location: any) => {
+    console.log(location);
+  };
   return (
     <View style={{ flex: 1 }}>
       <StatusBar style="light" />
@@ -35,26 +38,24 @@ const Home = () => {
               marginTop: 10,
             }}
           >
-
-            {
-              showSearch ? (
-                <TextInput
-                  placeholder="Search City"
-                  placeholderTextColor="white"
-                  style={{
-                    paddingLeft: 12,
-                    flex: 1,
-                    fontSize: 20,
-                    color: "white",
-                    height: "100%",
-                  }}
-                />
-
-              ) : null
-            }
+            {showSearch ? (
+              <TextInput
+                placeholder="Search City"
+                placeholderTextColor="white"
+                style={{
+                  paddingLeft: 12,
+                  flex: 1,
+                  fontSize: 20,
+                  color: "white",
+                  height: "100%",
+                }}
+              />
+            ) : null}
 
             <TouchableOpacity
-              onPress={() => { toggleSearch(!showSearch) }}
+              onPress={() => {
+                toggleSearch(!showSearch);
+              }}
               style={{
                 backgroundColor: "lightgray",
               }}
@@ -63,27 +64,143 @@ const Home = () => {
               <MagnifyingGlassCircleIcon size={38} color="white" />
             </TouchableOpacity>
           </View>
-          {
-            locations.length > 0 && showSearch ? (
-              <View className="absolute w-full bg-gray-300 mt-16 rounded-3xl">
-                {
-                  locations.map((loc, index) => {
-                    let showBorder = index + 1 != locations.length
-                    let borderClass= showBorder ?'border-b-2 border-b-gray-400': null
-                    return (
-                      <TouchableOpacity
-                        key={index}
-                        className={`flex-row items-center border-0 p-3 px-4 m-1 ${borderClass}`}
-                      >
-                        <MapPinIcon size={20} color="gray" />
-                        <Text>London, United Kingdom,</Text>
-                      </TouchableOpacity>
-                    )
-                  })
-                }
-              </View>
-            ):null
-          }
+          {locations.length > 0 && showSearch ? (
+            <View className="absolute w-full bg-gray-300 mt-16 rounded-3xl">
+              {locations.map((loc, index) => {
+                let showBorder = index + 1 != locations.length;
+                let borderClass = showBorder
+                  ? "border-b-2 border-b-gray-400"
+                  : null;
+                return (
+                  <TouchableOpacity
+                    onPress={() => handleLocation(loc)}
+                    key={index}
+                    className={`flex-row items-center border-0 p-3 px-4 m-1 ${borderClass}`}
+                  >
+                    <MapPinIcon size={20} color="gray" />
+                    <Text className="text-black text-lg ml-2">
+                      London, United Kingdom,
+                    </Text>
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+          ) : null}
+        </View>
+        {/* Showing forecast section */}
+
+        <View className="mx-4 flex justify-around flex-1 mb-2">
+          <Text className="text-white text-center text-2xl font-bold">
+            London, &nbsp;
+            <Text className="text-lg font-semibold text-gray-300 m-2">
+              United Kingdom
+            </Text>
+          </Text>
+          <View className="flex-row justify-center ">
+            <Image
+              className="w-52 h-52"
+              source={require("../assets/images/partlycloudy.png")}
+            />
+          </View>
+          <View className="space-y-2">
+            <Text className="text-center font-bold text-white text-6xl ml-5">
+              23&#176; C
+            </Text>
+            <Text className="text-center text-white text-xl tracking-widest">
+              Partly Cloud
+            </Text>
+          </View>
+          <View className="flex-row justify-between mx-4">
+            <View className="flex-row space-x-2 items-center">
+              <Image
+                source={require("../assets/icons/wind.png")}
+                className="h-6 w-6"
+              />
+              <Text className="text-white font-semibold text-base">22km</Text>
+            </View>
+            <View className="flex-row space-x-2 items-center">
+              <Image
+                source={require("../assets/icons/drop.png")}
+                className="h-6 w-6"
+              />
+              <Text className="text-white font-semibold text-base">23%</Text>
+            </View>
+            <View className="flex-row space-x-2 items-center">
+              <Image
+                source={require("../assets/icons/sun.png")}
+                className="h-6 w-6"
+              />
+              <Text className="text-white font-semibold text-base">
+                06:05 AM
+              </Text>
+            </View>
+          </View>
+        </View>
+        {/* Forecast section for next days */}
+        <View className="mb-2 space-y-3">
+          <View className="flex-row items-center mx-5 space-x-2">
+            <CalendarDaysIcon size={22} color="white" />
+            <Text className="text-white text-base">Daily Forecast</Text>
+          </View>
+          <ScrollView horizontal contentContainerStyle={{ paddingHorizontal: 15 }} showsHorizontalScrollIndicator={false}>
+            <View className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+              style={{ backgroundColor: "gray" }}>
+              <Image source={require('../assets/images/heavyrain.png')}
+                className="h-11 w-11"
+              />
+              <Text className="text-white">Monday</Text>
+              <Text className="text-white text-xl font-semibold">22&#176;C</Text>
+            </View>
+            <View className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+              style={{ backgroundColor: "gray" }}>
+              <Image source={require('../assets/images/heavyrain.png')}
+                className="h-11 w-11"
+              />
+              <Text className="text-white">Monday</Text>
+              <Text className="text-white text-xl font-semibold">22&#176;C</Text>
+            </View>
+            <View className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+              style={{ backgroundColor: "gray" }}>
+              <Image source={require('../assets/images/heavyrain.png')}
+                className="h-11 w-11"
+              />
+              <Text className="text-white">Monday</Text>
+              <Text className="text-white text-xl font-semibold">22&#176;C</Text>
+            </View>
+            <View className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+              style={{ backgroundColor: "gray" }}>
+              <Image source={require('../assets/images/heavyrain.png')}
+                className="h-11 w-11"
+              />
+              <Text className="text-white">Monday</Text>
+              <Text className="text-white text-xl font-semibold">22&#176;C</Text>
+            </View>
+            <View className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+              style={{ backgroundColor: "gray" }}>
+              <Image source={require('../assets/images/heavyrain.png')}
+                className="h-11 w-11"
+              />
+              <Text className="text-white">Monday</Text>
+              <Text className="text-white text-xl font-semibold">22&#176;C</Text>
+            </View>
+            <View className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+              style={{ backgroundColor: "gray" }}>
+              <Image source={require('../assets/images/heavyrain.png')}
+                className="h-11 w-11"
+              />
+              <Text className="text-white">Monday</Text>
+              <Text className="text-white text-xl font-semibold">22&#176;C</Text>
+            </View>
+            <View className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+              style={{ backgroundColor: "gray" }}>
+              <Image source={require('../assets/images/heavyrain.png')}
+                className="h-11 w-11"
+              />
+              <Text className="text-white">Monday</Text>
+              <Text className="text-white text-xl font-semibold">22&#176;C</Text>
+            </View>
+            
+          </ScrollView>
         </View>
       </SafeAreaView>
     </View>
